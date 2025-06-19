3ac28fed8cf7075d20828cb81709596383653bc9
YUZON, Tristan Justine M. <hed-tjyuzon@smu.edu.ph>
2025-06-19 08:22:54 +0800

    Remove server package.json and controllerHelper.js files to clean up the project structure.

--- server/_card/card.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cardController = require('./card.controller');
-
-// Route to create a business card
-router.post('/create-business-card', cardController.createBusinessCard);
-
-module.exports = router;
